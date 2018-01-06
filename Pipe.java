3bc9784bc026f3748d44ac3ef8891b5260318e24
LiLatee <30510627+LiLatee@users.noreply.github.com>
2018-01-06 22:57:07 +0100

    Update Pipe.java

--- Pipe.java
+++ Pipe.java
@@ -1,11 +1,9 @@
-package InterprocessCommunication;
-
 import java.util.LinkedList;
 
 public class Pipe
 {
 	String name;
-	private Boolean available = true;
+	MutexLock lock = new MutexLock();
 	LinkedList<Character> data = new LinkedList<Character>();
 	
 	public Pipe(String name)
@@ -13,32 +11,13 @@ public class Pipe
 		this.name = name;
 	}
 	
-	
 	public String getName()
 	{
 		return name;
-	}
-	public void setName(String name)
-	{
-		this.name = name;
-	}
-	public Boolean getAvailable()
-	{
-		return available;
-	}
-	public void setAvailable(Boolean available)
-	{
-		this.available = available;
 	}
 	public LinkedList<Character> getData()
 	{
 		return data;
 	}
-	public void setData(LinkedList<Character> data)
-	{
-		this.data = data;
-	}
-	
-	
-	
+
 }
