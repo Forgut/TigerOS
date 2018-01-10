7c90db50975e42a4e8b86cbb3b0ffeef4d7b8a08
LiLatee <30510627+LiLatee@users.noreply.github.com>
2018-01-11 00:05:05 +0100

    Add files via upload
    SYSTEM

---
+++ TigerOSv3/src/memorymanagement/FIFOFrame.java
@@ -0,0 +1,14 @@
+package memorymanagement;
+
+public class FIFOFrame {
+	protected int number;
+	protected int ID;
+	protected Boolean bit;
+	
+	public FIFOFrame(int number, int ID) {
+		this.number = number;
+		this.ID = ID;
+		this.bit = true;
+	}
+
+}
